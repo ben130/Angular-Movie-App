175e071328d902778378800d5df399654aef9074
ben130 <benherrman17@gmail.com>
2025-07-19 12:12:21 -0400

    Resolve Casing issue, Add More Sources

--- src/app/movie-form/movie-form.ts
+++ src/app/movie-form/movie-form.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { forkJoin, map, Observable } from 'rxjs';
+import { IMovie } from '../models/IMovie';
 
 @Component({
   selector: 'app-movie-form',
@@ -12,67 +13,17 @@ import { forkJoin, map, Observable } from 'rxjs';
 export class MovieForm {
   name: FormControl = new FormControl("");
   searchResults: any[] = [];
-  movies: any[] = [];
+  movies: IMovie[] = [];
   topRated: any[] = [];
-  randomMovie: any = {};
-  movies2020: any[] = [];
-  movies2010: any[] = [];
-  movies2000: any[] = [];
+  randomMovie: IMovie | undefined = undefined;
 
   constructor(private http: HttpClient) {
     
   }
 
   ngOnInit(){
-    console.log(this.name.value);
     this.loadContent();
     this.ngOnChanges()
-  }
-
-  loadContent() {
-    this.getMovies();
-    this.getTopRated();
-  }
-
-  getMovies() {
-    // this.http.get('assets/movies2020.json').subscribe((data: any) => {
-    //   this.movies2020 = data;
-    // });
-    // this.http.get('assets/movies-2010s.json').subscribe((data: any) => {
-    //   this.movies2010 = data;
-    //   console.log(this.movies2010)
-    // });            
-    this.getAllMovies().subscribe(data => {
-      this.movies = data;
-      console.log(this.movies)
-      console.log(this.movies.filter(x => x.Title == "Cars 2"))
-    });
-  }
-
-getAllMovies(): Observable<any[]> {
-  const files = ['movies2020.json', 'movies-2010s.json', 'movies-2000s.json'];
-  const requests: Observable<any[]>[] = files.map(name =>
-    this.http.get<any[]>(`assets/${name}`)
-  );
-  return forkJoin(requests).pipe(
-    map(results => results.flat())
-  );
-}
-
-  getTopRated(){
-    this.http.get('assets/top-rated.json').subscribe((data: any) => {
-      this.topRated = data;
-    });
-  }
-
-  getDetails() {
-    this.searchResults = this.name.value ? this.movies.filter(x => x.title.toString().includes(this.name.value)) : [];
-    console.log(this.movies.filter(x => x.title.includes(this.name.value)))
-  }
-
-  getRandomMovie() {
-    const index = Math.floor(Math.random() * this.movies.length);
-    this.randomMovie = this.movies[index];
   }
 
   ngOnChanges(){
@@ -80,4 +31,41 @@ getAllMovies(): Observable<any[]> {
       console.log(x);
     })
   }
+
+  loadContent() {
+    this.getMovies();
+    this.getTopRated();
+  }
+
+  getMovies() {     
+    this.getAllMovies().subscribe(data => {
+      this.movies = data;
+    });
+  }
+
+  getAllMovies(): Observable<IMovie[]> {
+    const files = ['movies2020.json', 'movies-2010s.json', 'movies-2000s.json', 'movies-1990s.json', 'movies-1980s.json'];
+    const requests: Observable<IMovie[]>[] = files.map(name =>
+      this.http.get<IMovie[]>(`assets/${name}`)
+    );
+    return forkJoin(requests).pipe(
+      map(results => results.flat())
+    );
+  }
+
+  getTopRated(){
+    this.http.get('assets/top-rated.json').subscribe((data: any) => {
+      this.topRated = data;
+    });
+  }
+
+  search() {
+    this.searchResults = this.name.value ? this.movies.filter(x => x.title.toLowerCase().includes(this.name.value.toLowerCase())) : [];
+  }
+
+  getRandomMovie() {
+    const index = Math.floor(Math.random() * this.movies.length);
+    this.randomMovie = this.movies[index];
+  }
 }
+
